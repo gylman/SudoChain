7bfed8cf4d807141b465988d69650fcde74e654f
gylman <gylmanbm@gmail.com>
2023-10-22 18:33:41 +0900

    pre-call

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,62 +1,88 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import BaseButton from './BaseButton';
+import { Link } from 'react-router-dom';
 
 const Wrapper = styled.div`
   position: absolute;
-  transform: translate(100%, 30%);
   display: flex;
+  top: 100px;
+  left: 50%;
+  transform: translateX(-50%);
   flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
 
-const TimerContainer = styled.div`
+const TopBar = styled.div`
   background: #ffffff;
   width: 100%;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  flex-direction: row;
   margin-bottom: 10px;
+  justify-content: space-between;
   border-radius: 10px;
   box-shadow: 0px 8px 16px 0px rgba(5, 74, 183, 0.2);
+  padding: 3px 20px;
 `;
 const Timer = styled.p`
   display: flex;
-  width: 100%;
-  justify-content: center;
+  width: auto;
   font-size: 28px;
   font-weight: bold;
   color: #304ff6;
   padding: 20px 10px;
 `;
-
+const Title = styled.span`
+  color: #4661e6;
+  font-weight: 700;
+  font-size: 36px;
+`;
+const Tag = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 77px;
+  height: 30px;
+  flex-shrink: 0;
+  border-radius: 10px;
+  background: #f2f4ff;
+  color: #4661e6;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-top: 16px;
+  margin-bottom: 16px;
+`;
 const Board81 = styled.div`
   display: grid;
   border-radius: 8px;
   border: 1px solid #d7e1f4;
   box-shadow: 0px 8px 16px 0px rgba(5, 74, 183, 0.2);
-  grid-template-columns: repeat(3, 186px);
+  grid-template-columns: repeat(3, 1fr);
   background: #d7e1f4;
   margin-bottom: 20px;
-  justify-content: center;
-  align-items: center;
+  gap: 6px;
 `;
 
 const Board9 = styled.div`
   display: grid;
-  width: auto;
-  border-radius: 8px;
-  grid-template-columns: repeat(3, 62px);
-  background: #d7e1f4;
-  justify-content: center;
-  align-items: center;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2px;
 `;
 
 const Cell = styled.input`
-  width: 60px;
-  height: 60px;
+  width: 62px; /* Use 100% to make it responsive within the grid */
+  height: 62px; /* Let it adjust based on the width for a square appearance */
   text-align: center;
   font-size: 24px;
   border: none;
+  padding: 20% 0; /* This helps maintain a square aspect ratio */
   color: #307df6;
+  box-sizing: border-box; /* To ensure padding doesn't expand it */
   &:focus {
     outline-color: blue;
   }
@@ -95,9 +121,11 @@ function Game() {
 
   return (
     <Wrapper>
-      <TimerContainer>
+      <TopBar>
+        <Title>SudoChain</Title>
         <Timer>18:30:11</Timer>
-      </TimerContainer>
+        <Tag>Easy</Tag>
+      </TopBar>
       <Board81>
         {board.map((big, index, arr) => {
           console.log('this2', arr);
@@ -121,7 +149,9 @@ function Game() {
           );
         })}
       </Board81>
-      <SubmitButton onClick={handleSubmit}>Submit</SubmitButton>
+      <Link to='/sudokus'>
+        <SubmitButton onClick={handleSubmit}>Submit</SubmitButton>
+      </Link>
     </Wrapper>
   );
 }
