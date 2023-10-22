f08ddef7e10bcd88574162bcdf943b3d1feca221
Cosmodude <vladislav.lenskii@gmail.com>
2023-10-23 01:26:40 +0900

    deployment argum,ents

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -2,13 +2,153 @@ import { ethers } from "hardhat";
 
 async function main() {
 
-  const initPoint1 = {
-      location: 0,
-      value:  7
-  }
+  const initPoints = [
+    {
+    location: 3,
+    value:  2
+    },
+    {
+        location: 4,
+        value:  6
+    },
+    {
+        location: 6,
+        value:  7
+    },
+    {
+        location: 8,
+        value:  1
+    },
+    {
+        location: 9,
+        value:  6
+    },
+    {
+        location: 10,
+        value:  8
+    },
+    {
+        location: 13,
+        value:  7
+    },
+    {
+        location: 16,
+        value:  9
+    },
+    {
+        location: 18,
+        value:  1
+    },
+    {
+        location: 19,
+        value:  9
+    },
+    {
+        location: 23,
+        value:  4
+    },
+    {
+        location: 24,
+        value:  5
+    },
+    {
+        location: 27,
+        value:  8
+    },
+    {
+        location: 28,
+        value:  2
+    },
+    {
+        location: 30,
+        value:  1
+    },
+    {
+        location: 34,
+        value:  4
+    },
+    {
+        location: 38,
+        value:  4
+    },
+    {
+        location: 39,
+        value:  6
+    },
+    {
+        location: 41,
+        value:  2
+    },{
+        location: 42,
+        value:  9
+    },
+    {
+        location: 46,
+        value:  5
+    },
+    {
+        location: 50,
+        value:  3
+    },
+    {
+        location: 52,
+        value:  2
+    },
+    {
+        location: 53,
+        value:  8
+    },
+    {
+        location: 56,
+        value:  9
+    },
+    {
+        location: 57,
+        value:  3
+    },
+    {
+        location: 61,
+        value:  7
+    },
+    {
+        location: 62,
+        value:  4
+    },
+    {
+        location: 64,
+        value:  4
+    },
+    {
+        location: 67,
+        value:  5
+    },
+    {
+        location: 70,
+        value:  3
+    },
+    {
+        location: 71,
+        value:  6
+    },
+    {
+        location: 72,
+        value:  7
+    },
+    {
+        location: 74,
+        value:  3
+    },
+    {
+        location: 75,
+        value:  1
+    },
+    {
+        location: 76,
+        value:  8
+    },
+]
 
-
-  const sudoku = await ethers.deployContract("SudoChain",  [[initPoint1,]] );
+  const sudoku = await ethers.deployContract("SudoChain",  [initPoints] );
 
   await sudoku.waitForDeployment();
 
